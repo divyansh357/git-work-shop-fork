88a358477281ba2ad388a1feaa10c10edde08212
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-07-08 11:12:31 +0530

    Modified command.sql

--- command.sql
+++ command.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS employee (
     emp_id INT PRIMARY KEY,
     emp_name VARCHAR(100),
+    emp_gender VARCHAR(50),
     department VARCHAR(50),
-    salary DECIMAL(10,2)
+    salary DECIMAL(10,2),
 );
