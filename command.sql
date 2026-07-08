ec920c7e1ec1319c39439851ffa4f7bb70432537
Divyansh Kumar Gupta <divyanshkumargupta532@gmail.com>
2026-07-08 12:16:03 +0530

    Performing cherry pick demo

--- command.sql
+++ command.sql
@@ -1,5 +1,13 @@
+
 CREATE TABLE IF NOT EXISTS student (
     student_id INT,
     student_name VARCHAR(50),
     marks DECIMAL(5,2)
 }
+
+CREATE TABLE IF NOT EXISTS employee (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(100),
+    emp_gender VARCHAR(50),
+    emp_dept VARCHAR(50)
+);
